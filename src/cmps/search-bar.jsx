09833e31c5f8bb34c51ed9d38d7c518ba61f3673
mayalmog <mayalmog@gmail.com>
2022-09-19 18:58:20 +0300

    cleaning

--- src/cmps/search-bar.jsx
+++ src/cmps/search-bar.jsx
@@ -5,11 +5,8 @@ import { httpService } from "../service/http.service";
 import { setQuery, setResults } from "../store/youtube/youtubeSlice";
 
 export const SearchBar = () => {
-  // const currQuery = useSelector((state) => state.youtube.query);
-  // const currResults = useSelector((state) => state.youtube.results);
   const dispatch = useDispatch();
   const [searchQuery, setSearchQuery] = useState(null);
-  // const [localResults, setLocalResults] = useState(null);
 
   const handleChange = ({ target }) => {
     const { value } = target;
@@ -22,8 +19,6 @@ export const SearchBar = () => {
     dispatch(setQuery({ searchQuery }));
     const results = await httpService.getYouTubeResults(searchQuery);
     console.log("results: ", results);
-    // setLocalResults(results);
-    // console.log(localResults);
     dispatch(setResults(results));
   };
 
@@ -35,11 +30,6 @@ export const SearchBar = () => {
         </label>
         <input type="submit" value="Submit" onClick={getResults} />
       </form>
-      {/* <p>local query: {searchQuery}</p> */}
-      {/* <p>Store query: {currQuery}</p> */}
-      {searchQuery && <pre>{JSON.stringify(searchQuery, null, 2)}</pre>}
-      {/* {currQuery && <pre>{JSON.stringify(currQuery, null, 2)}</pre>} */}
-      {/* {currResults && <pre>{JSON.stringify(currResults, null, 2)}</pre>} */}
     </section>
   );
 };
--- src/store/youtube/youtubeSlice.js
+++ src/store/youtube/youtubeSlice.js
@@ -1,22 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { httpService } from "../../service/http.service";
-
-// const initialState = {
-//   query: "",
-//   results: [],
-//   loading: false,
-// };
-
-// const getYouTubeResults = createAsyncThunk(
-//   //action type string
-//   "youtube/getYouTubeResults",
-//   // callback function
-//   async () => {
-//     const res = await httpService.getYouTubeResults(youtubeSlice.query);
-//     console.log(res);
-//     return res;
-//   }
-// );
 
 export const youtubeSlice = createSlice({
   name: "youtube",
@@ -33,18 +15,6 @@ export const youtubeSlice = createSlice({
       state.results = action.payload;
     },
   },
-  // extraReducers: {
-  //   [getYouTubeResults.pending]: (state) => {
-  //     state.loading = true;
-  //   },
-  //   [getYouTubeResults.fulfilled]: (state, { payload }) => {
-  //     state.loading = false;
-  //     state.results = payload;
-  //   },
-  //   [getYouTubeResults.rejected]: (state) => {
-  //     state.loading = false;
-  //   },
-  // },
 });
 
 export const { setQuery, setResults } = youtubeSlice.actions;
